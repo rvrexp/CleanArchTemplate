6a971290836fe9040ffb65b7b187a757ae0d4eba
Richu Raju <richurajuvaz@outlook.com>
2024-11-19 19:00:02 -0600

    finaltest

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace WhiteLagoon.Web.Controllers
         {
             int t = 2;
             string tr = "";
+            string get = "test";
             return View();
             
         }
